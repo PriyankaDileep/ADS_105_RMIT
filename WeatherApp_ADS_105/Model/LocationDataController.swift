8239d9c1f72f2f73180ee595787db6a1b7710db2
Priyanka <priyauthram@yahoo.co.in>
2021-04-22 12:17:48 +1000

    Added 20 geolocations in LocationDataController

---
+++ WeatherApp_ADS_105/Model/LocationDataController.swift
@@ -0,0 +1,24 @@
+
+final class LocatioDataController {
+    
+    static let locations:[Location] =  [Location(name: "Melbourne", latitude: "-37.813629", longitude: "144.963058"),
+                                        Location(name: "Brisbane", latitude: "-27.469770", longitude: "153.025131"),
+                                        Location(name: "Darwin", latitude: "-12.463440", longitude: "130.845642"),
+                                        Location(name: "Hobart", latitude: "-42.881901", longitude: "147.323807"),
+                                        Location(name: "Adelaide", latitude: "-34.927170", longitude: "138.599533"),
+                                        Location(name: "Sydney", latitude: "", longitude: ""),
+                                        Location(name: "Stockholm", latitude: "59.3294", longitude: "18.0686"),
+                                        Location(name: "Gothenburg", latitude: "57.6717", longitude: "11.9810"),
+                                        Location(name: "London", latitude: "51.5072", longitude: "-0.1275"),
+                                        Location(name: "Birmingham", latitude: "52.4800", longitude: "-1.9025"),
+                                        Location(name: "Manchester", latitude: "53.4794", longitude: "-2.2453"),
+                                        Location(name: "Newcastle", latitude: "55.0077", longitude: "-1.6578"),
+                                        Location(name: "Liverpool", latitude: "53.4000", longitude: "-2.9833"),
+                                        Location(name: "Portsmouth", latitude: "50.8058", longitude: "-1.0872"),
+                                        Location(name: "Southampton", latitude: "50.9000", longitude: "-1.4000"),
+                                        Location(name: "Nottingham", latitude: "52.9500", longitude: "-1.1500"),
+                                        Location(name: "Edinburgh", latitude: "55.9530", longitude: "-3.1890"),
+                                        Location(name: "Middlesbrough", latitude: "54.5767", longitude: "-1.2355"),
+                                        Location(name: "Warrington", latitude: "53.3917", longitude: "-2.5972"),
+                                        Location(name: "Oxford", latitude: "51.7519", longitude: "-1.2578")]
+}
